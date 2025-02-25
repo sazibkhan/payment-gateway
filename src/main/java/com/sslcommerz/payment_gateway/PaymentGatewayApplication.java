2109ef081d134d97b4f36e1a0610825cae2f18da
Md. Sazib Khan/TECH <sazib.khan@akashdth.com>
2025-02-25 10:13:29 +0600

    Payment Gateway Running..!

---
+++ src/main/java/com/sslcommerz/payment_gateway/PaymentGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.sslcommerz.payment_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayApplication.class, args);
+		System.out.println("Payment Gateway Running..! ");
+	}
+
+}
